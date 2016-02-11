3a7aa1003518efe5015af509e59ece184b499df9
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-02-11 16:55:18 +0000

    Add views for devise.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.precompile += %w( home.jpg )
+Rails.application.config.assets.precompile += %w( musikore.svg )
+Rails.application.config.assets.precompile += %w( musikore-com.svg )
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
