a92045f25e9c9cb59b1cc95e473a9f29c272c1f6
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-02-17 13:06:23 +0000

    Validate gender selections, add log out feature.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  validates_inclusion_of :gender, :in => %w(M F U)
 end
