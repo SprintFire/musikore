cf09779d3fa8754e3f90a05f4523016f39a9e2dc
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-02-18 22:03:49 +0000

    Add validation of Username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+  # Validations
   validates_inclusion_of :gender, :in => %w(M F U), allow_nil: true, allow_blank: true
+  validates :username, uniqueness: true
 end
