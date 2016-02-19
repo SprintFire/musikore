8d953309820dd133c9fe5ef90ff0ff0aa6849544
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-02-19 02:06:54 +0000

    Validate username, fix bugs of Auth.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Validations
-  validates_inclusion_of :gender, :in => %w(M F U), allow_nil: true, allow_blank: true
-  validates :username, uniqueness: true
+  validates_inclusion_of :gender, :in => %w(M F U), allow_nil: true, allow_blank: false
+  validates :username, presence: true, uniqueness: true, allow_nil: false
 end
