021fafb401a1e930e7dbdc769b8e3cfe3247feb6
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-02-29 19:18:39 +0000

    Edit banners.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,4 +3,15 @@ class UserController < ApplicationController
     #@user = User.find(params[:id])
     @user = User.find_by(username: params[:username])
   end
+
+  def update
+    @user = current_user
+
+    if @user.update(user_params)
+      flash[:success] = "Your account has been successfully updated!"
+      render 'edit'
+    else
+      render 'edit'
+    end
+  end
 end
